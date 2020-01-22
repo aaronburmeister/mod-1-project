59cfe1138fb15612eea9ff5b0b2c6ff23076c578
Michael Newman <coloradohokie@gmail.com>
2020-01-21 18:06:10 -0700

    updated seed data

--- seeds.rb
+++ seeds.rb
@@ -1,41 +1,19 @@
 table: destinations:
 
-name: Denver
-description: The City and County of Denver, is the capital and most populous municipality of the U.S. state of Colorado. Denver is located in the South Platte River Valley on the western edge of the High Plains just east of the Front Range of the Rocky Mountains. With an estimated population of 716,492 in 2018, Denver is the 19th-most populous U.S. city, and with a 19.38% increase since the 2010 United States Census, it has been one of the fastest-growing major cities in the United States.[17] The metropolitan city is located in the middle of the extensive Southern Rocky Mountain Front with Cheyenne, Wyoming, to the north and Albuquerque, New Mexico, to the south, this rapidly growing region has a population of 5,467,633, according to the 2010 United States Census. The Denver downtown district is immediately east of the confluence of Cherry Creek with the South Platte River, approximately 12 mi (19 km) east of the foothills of the Rocky Mountains. Denver is named after James W. Denver, a governor of the Kansas Territory. It is nicknamed the Mile High City because its official elevation is exactly one mile (5280 feet or 1609.3 meters) above sea level.[18] The 105th meridian west of Greenwich, the longitudinal reference for the Mountain Time Zone, passes directly through Denver Union Station.
-latitude: 39 45 43
-longitude: -104 52 52
+Destination.create(name: "Denver" description: "The City and County of Denver, is the capital and most populous municipality of the U.S. state of Colorado." latitude: 39.73915 longitude: -104.9847)
+
+Destination.create(name:"Aspen" description: "Founded as a mining camp during the Colorado Silver Boom and later named Aspen because of the abundance of aspen trees in the area, the city boomed during the 1880s, its first decade of existence." latitude: 39.1911 longitude: -106.81754)
+
+Destination.create(name: "Palisade" description: "Palisade is a Statutory Town in Mesa County, Colorado, United States." latitude: 39.1911 longitude: -106.81754)
+
+Destination.create(name: "Rocky Mountain National Park" description: "Rocky Mountain National Park is an American national park located approximately 76 mi (122 km) northwest of Denver International Airport." latitude: 39.613357 longitude: -105.286243)
 
 
-name: Aspen
-description: Founded as a mining camp during the Colorado Silver Boom and later named "Aspen" because of the abundance of aspen trees in the area, the city boomed during the 1880s, its first decade of existence. The boom ended when the Panic of 1893 led to a collapse in the silver market, and the city began a half-century known as "the quiet years" during which its population steadily declined, reaching a nadir of fewer than a thousand by 1930. Aspen's fortunes reversed in the mid-20th century when neighboring Aspen Mountain was developed into a ski resort, and industrialist Walter Paepcke bought many properties in the city in the 1950s and redeveloped them. Today it is home to three institutions, two of which Paepcke helped found, that have international importance: the Aspen Music Festival and School, the Aspen Institute, and the Aspen Center for Physics.
-latitude: 39 11 42
-longitude: -106 50 13
-
-name: Palisade
-description: Palisade is a Statutory Town in Mesa County, Colorado, United States. It is part of the Grand Junction Metropolitan Statistical Area. The population was 2,692 at the 2010 census. The community was named for the cliffs near the town site.[8] Palisade is known for its peach orchards and wine vineyards. The climate, a 182-day growing season, and an average 78 percent of sunshine makes Palisade "The Peach Capital of Colorado".
-latitude: 39 06 34
-longitude: -108 21 15
-
-name: Rocky Mountain National Park
-description: Rocky Mountain National Park is an American national park located approximately 76 mi (122 km) northwest of Denver International Airport[5] in north-central Colorado, within the Front Range of the Rocky Mountains. The park is situated between the towns of Estes Park to the east and Grand Lake to the west. The eastern and western slopes of the Continental Divide run directly through the center of the park with the headwaters of the Colorado River located in the park's northwestern region.[6] The main features of the park include mountains, alpine lakes and a wide variety of wildlife within various climates and environments, from wooded forests to mountain tundra.
-latitude: 40 20 00
-longitude: -105 42 32
-
-table activities
-name: skiing
-
-name: hiking
-
-name: Shopping
-
-name: Nightlife
-
-name: Breweries
-
-name: Museums
-
-name: Wildlife Viewing
-
-name: Wineries
-
-table destination_activities
+Activity.create (name: "Skiing")
+Activity.create (name: "Hiking")
+Activity.create (name: "Shopping")
+Activity.create (name: "Nightlife")
+Activity.create (name: "Breweries")
+Activity.create (name: "Museums")
+Activity.create (name: "Wildlife Viewing")
+Activity.create (name: "Wineries")
