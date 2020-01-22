db6dd4bdd3a4457932ec63caff13af9904d7c5b5
Michael Newman <coloradohokie@gmail.com>
2020-01-22 15:48:03 -0700

    updated main menu to include options based on the functions we currently have

--- lib/cli.rb
+++ lib/cli.rb
@@ -21,13 +21,28 @@ class Cli
         array = Destination.all.map do |destination|
             destination.name
         end
-        user.location = prompt.select("Where are you?", array)
-        activity_at_location(user.location)
-        destination_with_activity("Hiking")
+        main_menu_options = ["See Activities at my location", "See Activities at another location"]
+        user_action = prompt.select("What do you want to do?", main_menu_options)
+        
+        if user_action == "See Activities at my location"
+            user.location = prompt.select("Where are you?", array)
+            puts "Here is the list all you can do in #{user.location}:"
+            activity_at_location(user.location)
+        end
+
+        if user_action == "See Activities at another location"
+            options = Activity.all.map do |activity|
+                activity.name
+            end
+            user_activity = prompt.select("What do you want to do?", options)
+            puts "Here are the locations with the following activity:"
+            destination_with_activity(user_activity)
+        end
+        
     end
 
     def activity_at_location(location)
-        puts "Here is the list all you can do in #{location}:"
+        # puts "Here is the list all you can do in #{location}:"
         activities = Destination.find_by(name: location).activities
 
         list = activities.map do |activity|
@@ -37,7 +52,7 @@ class Cli
     end
 
     def destination_with_activity(activity)
-        puts "Here is all the location with the following activity"
+        # puts "Here is all the location with the following activity"
         destinations = Activity.find_by(name: activity).destinations
 
         list = destinations.map do |destination|
