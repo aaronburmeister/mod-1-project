b0009f539b44d40e31f2d4b7ee37928541d4564b
github username <Paradafaii28@gmail.com>
2020-01-21 18:17:54 -0700

    add seed to database

--- config/environment.rb
+++ config/environment.rb
@@ -2,3 +2,26 @@ require 'bundler/setup'
 Bundler.require
 
 require_all 'lib'
+
+
+# Destination.create(name: "Denver", description: "The City and County of Denver, is the capital and most populous municipality of the U.S. state of Colorado.", latitude: 39.73915, longitude: -104.9847)
+
+# Destination.create(name:"Aspen", description: "Founded as a mining camp during the Colorado Silver Boom and later named Aspen because of the abundance of aspen trees in the area, the city boomed during the 1880s, its first decade of existence.", latitude: 39.1911, longitude: -106.81754)
+
+# Destination.create(name: "Palisade", description: "Palisade is a Statutory Town in Mesa County, Colorado, United States.", latitude: 39.1911, longitude: -106.81754)
+
+# Destination.create(name: "Rocky Mountain National Park", description: "Rocky Mountain National Park is an American national park located approximately 76 mi (122 km) northwest of Denver International Airport.", latitude: 39.613357, longitude: -105.286243)
+
+
+# Activity.create(name: "Skiing")
+# Activity.create(name: "Hiking")
+# Activity.create(name: "Shopping")
+# Activity.create(name: "Nightlife")
+# Activity.create(name: "Breweries")
+# Activity.create(name: "Museums")
+# Activity.create(name: "Wildlife Viewing")
+# Activity.create(name: "Wineries")
+
+binding.pry
+0
+
